cc4dd4b6f249bcdf06a21b5164d4e771c9661c7b
Flame787 <mbrezovic77@gmail.com>
2025-01-29 17:46:09 +0100

    added basis state to show questions and answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,45 @@
 import { useState } from "react";
 
-export default function Quiz(){
+import QUESTIONS from "../questions.js";
 
-// state-logic to manage currently active question, change to different question, store the answers 
+export default function Quiz() {
+  // state-logic to manage currently active question, change to different question, store the answers
 
-const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  // -> this state could be derrived from the answers-state,
+  // because all answers are connected to the questions in the questions.js file
 
-const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
 
-    return <p>Currently active question</p>
+  // derrived state, a computed value to this component, it refers on the index/count of the answers:
+  // if activeQuestionIndex is 0, then we are showing the 1st question to the user,
+  // next index = 1 ==> this means 2nd question etc.
+  // We want to manage as little state as possible, and DERRIVE as much state as possible.
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+    // function is taking the selected answer as a parameter/value, and then is updating the state
+    // updating state: copiyng all previous state + adding newly selected answer
+  }
+
+  // return <p>Currently active question</p>
+  return (
+    <div id="quiz">
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            {/* function is not just passed, but is included inside of an anonymous function, so tha
+            we can have more control. It will not be called immidietly, but only when the button is clicked. */}
+          </li>
+        ))}
+      </ul>
+    </div>
+    </div>
+  );
 }
